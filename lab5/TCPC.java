b35264732c889a29a20800ecc48671559d6abe97
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-01-06 15:23:53 +0530

    Update TCPC.java

--- lab5/TCPC.java
+++ lab5/TCPC.java
@@ -2,29 +2,39 @@
 
 import java.net.*;
 import java.io.*; 
-  public class TCPC
-    {
-       public static void main(String[] args) throws Exception
-       {
-         Socket sock=new Socket("127.0.01", 4000);
-          System.out.println("Enter the filename");
-         BufferedReader keyRead=new BufferedReader(new InputStreamReader(System.in));
-         String fname=keyRead.readLine();
-         OutputStream ostream=sock.getOutputStream();
-         PrintWriter pwrite=new PrintWriter(ostream, true);
-         pwrite.println(fname);
-         InputStream istream=sock.getInputStream();
-         BufferedReader socketRead=new BufferedReader(new InputStreamReader(istream));
-         String str;
-         while((str=socketRead.readLine())!=null)
-           {
-              System.out.println(str);
-           }
-         pwrite.close();
-         socketRead.close();
-         keyRead.close();
-       }
-    }       
+
+public class TCPC {
+    public static void main(String[] args) throws Exception {
+        // Creating a socket to connect to the server on localhost (127.0.0.1) at port 4000
+        Socket sock = new Socket("127.0.0.1", 4000);
+        
+        // Reading the filename from the user
+        System.out.println("Enter the filename");
+        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+        String fname = keyRead.readLine();
+        
+        // Getting the output stream from the socket to send the filename to the server
+        OutputStream ostream = sock.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
+        pwrite.println(fname);
+        
+        // Getting the input stream from the socket to read the content sent by the server
+        InputStream istream = sock.getInputStream();
+        BufferedReader socketRead = new BufferedReader(new InputStreamReader(istream));
+        String str;
+        
+        // Reading and printing the content received from the server
+        while ((str = socketRead.readLine()) != null) {
+            System.out.println(str);
+        }
+        
+        // Closing resources
+        pwrite.close();
+        socketRead.close();
+        keyRead.close();
+    }
+}
+       
        
 /*First run the server program then in new terminal window run the client program and input the 
 file name the server will return the file content if it exists
