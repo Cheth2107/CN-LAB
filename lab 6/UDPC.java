37f8910aad7ed6c1669ca4e29219b6bfc2589aa3
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-01-08 19:40:51 +0530

    Update UDPC.java

--- lab 6/UDPC.java
+++ lab 6/UDPC.java
@@ -1,28 +1,47 @@
 /*UDP cleint program*/
 
-import java.io.*; 
+import java.io.*;
 import java.net.*;
-     public class UDPClient
-         {
-           public static void main(String[] args) throws Exception
-         {
-          BufferedReader inFromUser=new BufferedReader(new InputStreamReader(System.in));
-          DatagramSocket clientSocket=new DatagramSocket();
-          InetAddress IPAddress=InetAddress.getByName("localhost");
-          byte[] sendData = new byte[1024];
-          byte[] receiveData=new byte[1024];
-          System.out.println("Enter the string in lowercase so that you receive the message in Uppercase from the server");
-          String sentence=inFromUser.readLine();
-          sendData=sentence.getBytes();
-         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9884);
-          clientSocket.send(sendPacket);
-          DatagramPacket receivePacket=new DatagramPacket(receiveData, receiveData.length);
-          clientSocket.receive(receivePacket);
-          String modifiedSentence=new String(receivePacket.getData());
-          System.out.println("FROM SERVER: "+modifiedSentence);
-          clientSocket.close();
-               
-          }
+
+public class UDPClient {
+    public static void main(String[] args) throws Exception {
+        // Creating a BufferedReader to read input from the user
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        // Creating a DatagramSocket for the client
+        DatagramSocket clientSocket = new DatagramSocket();
+
+        // Getting the IP address of the server (localhost in this case)
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+
+        // Arrays to hold the data for sending and receiving
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+
+        // Prompting the user to enter a string in lowercase
+        System.out.println("Enter the string in lowercase so that you receive the message in Uppercase from the server");
+        String sentence = inFromUser.readLine();
+
+        // Converting the string to bytes and creating a DatagramPacket for sending to the server
+        sendData = sentence.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9884);
+
+        // Sending the packet to the server
+        clientSocket.send(sendPacket);
+
+        // Creating a DatagramPacket for receiving data from the server
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+        // Receiving the modified data from the server
+        clientSocket.receive(receivePacket);
+        String modifiedSentence = new String(receivePacket.getData());
+
+        // Printing the modified data received from the server
+        System.out.println("FROM SERVER: " + modifiedSentence);
+
+        // Closing the client socket
+        clientSocket.close();
+    }
 }
 
 /*output;-
